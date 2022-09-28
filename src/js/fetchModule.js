5ea0b50c8f2b52d5ed1750b82b24c8e888556eae
Julia-Frost <juliafrostwork@gmail.com>
2022-09-28 22:17:55 +0300

    add files

---
+++ src/js/fetchModule.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const API_WEB = 'https://pixabay.com/api/';
+const API_KEY = '?key=30054612-8d338f00f07d687f9e2b96ac6&';
+const API_OPTIONS =
+  '&image_type=photo&orientation=horizontal&safesearch=true&per_page=';
+const API_ORDER = 40;
+
+export default class PhotoApiService {
+  constructor() {
+    this.needToFind = '';
+    this.page = 1;
+  }
+
+  fetchPhotos() {
+    try {
+      const responce = axios.get(
+        `${API_WEB}${API_KEY}q=${this.needToFind}${API_OPTIONS}${API_ORDER}&page=${this.page}`
+      );
+      const data = responce.data;
+
+      this.addPhotos();
+
+      return data;
+    } catch (error) {
+      console.log('Error on try...catch', error);
+    }
+  }
+
+  addPhotos() {
+    this.page += 1;
+  }
+
+  resetPhotos() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.needToFind;
+  }
+
+  set query(newQuery) {
+    this.needToFind = newQuery;
+  }
+}
---
+++ src/js/resultModule.js
@@ -0,0 +1,82 @@
+import Notiflix from 'notiflix';
+import PhotoApiService from './fetchModule';
+
+const formEl = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+formEl.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+const photoApiService = new PhotoApiService();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  photoApiService.query = event.currentTarget.elements.searchQuery.value.trim();
+  photoApiService.resetPhotos();
+
+  if (photoApiService.query === '') {
+    loadMoreBtn.style.cssText = 'visibility: hidden;';
+    gallery.innerHTML = '';
+    return Notiflix.Notify.warning('Please enter 1 character!');
+  }
+
+  photoApiService
+    .fetchPhotos()
+    .then(data => {
+      if (data.total > 1) {
+        gallery.innerHTML = '';
+        createPhotoEl(data.hits);
+        Notiflix.Notify.success(`We find you photo!`);
+        Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+        loadMoreBtn.style.cssText = 'visibility: visible';
+      }
+      if (data.total === 0) {
+        gallery.innerHTML = '';
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+        loadMoreBtn.style.cssText = 'visibility: hidden;';
+      }
+      if (data.total <= 40) {
+        loadMoreBtn.style.cssText = 'visibility: hidden;';
+      }
+    })
+    .catch('Error on function');
+}
+
+async function onLoadMore() {
+  await photoApiService
+    .fetchPhotos()
+    .then(data => {
+      createPhotoEl(data.hits);
+    })
+    .catch('Error on function', error);
+  Notiflix.Notify.success(`You load next photos page!`);
+}
+
+function createPhotoEl(photos) {
+  const photoList = photos.map(photo => {
+    return `<div class="photo-card">
+  <img src="${photo.largeImageURL}" alt="${photo.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes <span>${photo.likes}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Views <span>${photo.views}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Comments <span>${photo.comments}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Downloads <span>${photo.downloads}</span></b>
+    </p>
+  </div>
+</div>`;
+  });
+  photoList.forEach(markupPhoto => {
+    gallery.insertAdjacentHTML('beforeend', markupPhoto);
+  });
+}
